94d9f971f6aaf97bb8edac5a601d19895b954ff9
Jerry Iu <jerryiu424@hotmail.com>
2019-01-25 12:57:18 -0500

    Jerry's push to personal github

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 
 from flask import Flask, render_template, url_for, redirect
 from flask_dance.contrib.facebook import make_facebook_blueprint, facebook
+from flask_login import logout_user 
 
 from flask_dance.consumer import OAuth2ConsumerBlueprint
 from functools import partial
@@ -73,7 +74,6 @@ import requests
 
 # facebook = LocalProxy(partial(_lookup_app_object, "facebook_oauth"))
 
-
 app = Flask(__name__)
 app.secret_key = "supersekrit"
 
@@ -83,27 +83,67 @@ blueprint = make_facebook_blueprint(
 )
 app.register_blueprint(blueprint)
 
-@app.route('/')
+@app.route('/login')
 def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+    if not facebook.authorized:
+        return redirect(url_for("facebook.login"))
 
-    return render_template('index.html', times=dummy_times)
+    return render_template('index.html')
 
-@app.route("/login")
+@app.route('/logout')
+
+def logout():
+    logout_user()
+    return redirect(url_for("facebook.login"))
+
+
+@app.route("/")
 def login():
     if not facebook.authorized:
         return redirect(url_for("facebook.login"))
-    redirect(url_for("facebook.login"))
-    resp = blueprint.session.get("/me?scope=email&fields=id,address,email,first_name,last_name,name,gender,locale,hometown,birthday,picture")
-    print(facebook.__dict__['token']['access_token'])
-    print(resp.json()['email'])
 
-    return "You are @{screen_name} on facebook".format(screen_name=resp.json()["name"])
+    resp = blueprint.session.get("/me?scope=email&fields=id,address,email,first_name,last_name,name,gender,locale,hometown,birthday,picture")
+
+    db_results = requests.get("https://jmb.im/uofthacks/add_to_db.php?uid=" + resp.json()['id'] + "&fname=" + resp.json()['first_name'] + "&sname=" + resp.json()['last_name'] + "&email=" + resp.json()['first_name'] + resp.json()['last_name'] + "@facebook.com")
+    subList_results = requests.get("https://jmb.im/uofthacks/get_list_of_subs.php?uid=" + resp.json()['id'])
+
+    actualScript = ""
+    subDict = {"netflix" : "Netflix", "spotify" : "Spotify", "hulu" : "Hulu", "apple_music" : "Apple Music", "youtube_premium" : "Youtube Music"}
+    people_results = requests.get("https://jmb.im/uofthacks/get_all_users.php")
+    people_results = people_results.text
+
+    for sub in subList_results.json():
+        sub_results = (requests.get("https://jmb.im/uofthacks/get_sub_info.php?id=" + sub)).json()
+        sub_id = sub_results['sub_id']
+        service = sub_results['service']
+        fee = str(float(sub_results['fee'])/100)
+        due = sub_results['due']
+
+        sub_people_results = (requests.get("https://jmb.im/uofthacks/get_users_sharing_sub.php?id=" + sub_id)).json()
+        sub_people_list = ""
+        
+        for subPeople in sub_people_results:
+            sharedPeople_results = (requests.get("https://jmb.im/uofthacks/get_name_from_uid.php?uid=" + subPeople)).json()
+            sub_people_list = sharedPeople_results["fname"] + " " + sharedPeople_results["sname"] + " (" + sharedPeople_results["email"] + ")" + ", " + sub_people_list
+
+        people = people_results
+
+        icon = "https://jmb.im/uofthacks/icons/" + service + ".png"
+        html_Cards_Stuff = ("<div class=\"card bill\"" +
+                            "onclick=\"openModal(" + fee + "," + due + "," + sub_id +")\"> <div class=\"card-body\">" +
+                            "<div class=\"row\"> <div class=\"col-12 col-md-2\"> <img src=\"" + icon + "\" />" +
+                            "</div> <div class=\"col-12 col-md-10\"> <h2>" + subDict[service] + "</h2>" +
+                            "<p>You owe: $" + fee +" </p> <small>Due in " + due + " days</small>" + 
+                            "</div></div></div></div>")
+        actualScript = actualScript + html_Cards_Stuff
+
+
+        
+
+    return render_template('index.html', name = resp.json()['name'], picture=resp.json()['picture']['data']['url'], uid=resp.json()['id'] ,fee = fee, due = due, people = people_results, sharedList = sub_people_list, ScriptStuff = actualScript)
+    
+
+
 
 
 if __name__ == '__main__':
